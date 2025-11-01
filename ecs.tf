0a6a9637ad5e8c54117d5f7108ca3f0a5d765c18
luqmannnn <luqmannurhakimt@gmail.com>
2025-11-01 11:23:24 +0800

    Update workflow

---
+++ ecs.tf
@@ -0,0 +1,70 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+terraform {
+  backend "s3" {
+    bucket = "sctp-ce11-tfstate"
+    key    = "luqman-s3-tf-cicd.tfstate" #Change this
+    region = "us-east-1"
+  }
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "6.17.0"
+    }
+  }
+
+  required_version = ">= 1.0.0"
+}
+
+data "aws_caller_identity" "current" {}
+
+locals {
+  prefix = split("/", data.aws_caller_identity.current.arn)[1]
+  account_id  = data.aws_caller_identity.current.account_id
+}
+
+resource "aws_ecr_repository" "ecr" {
+  name         = "${local.prefix}-ecr"
+  force_delete = true
+}
+
+module "ecs" {
+  source  = "terraform-aws-modules/ecs/aws"
+  version = "~> 5.9.0"
+
+  cluster_name = "${local.prefix}-ecs"
+  fargate_capacity_providers = {
+    FARGATE = {
+      default_capacity_provider_strategy = {
+        weight = 100
+      }
+    }
+  }
+
+  services = {
+    luqman-ce11-task-def = { #task definition and service name -> #Change
+      cpu    = 512
+      memory = 1024
+      container_definitions = {
+        luqman-ce11-container = { #container name -> Change
+          essential = true
+          image     = "${data.aws_caller_identity.current.account_id}.dkr.ecr.${data.aws_region.current.name}.amazonaws.com/${locals.prefix}-ecr:latest"
+          port_mappings = [
+            {
+              containerPort = 8080
+              protocol      = "tcp"
+            }
+          ]
+        }
+      }
+      assign_public_ip                   = true
+      deployment_minimum_healthy_percent = 100
+      subnet_ids                   = [] #List of subnet IDs to use for your tasks
+      security_group_ids           = [] #Create a SG resource and pass it here
+    }
+  }
+}
+
